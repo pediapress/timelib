1641c2908fdca61a1d7e1940a8fee6a60c203770
ralf schmitt <schmir@gmail.com>
2009-06-24 14:19:04 +0200

    if setuptools is available, try to use it and pass zip_safe=False.

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #! /usr/bin/env python
 
-from distutils.core import setup
-from distutils.extension import Extension
-
+try:
+    from setuptools import setup, Extension    
+    extra = dict(zip_safe=False)
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+    extra = dict()
+    
 sources = "astro.c dow.c parse_date.c parse_tz.c timelib.c tm2unixtime.c unixtime2tm.c".split()
 sources = ["ext-date-lib/"+x for x in sources]
 sources.append("timelib.c")
 
 setup(name='timelib',
-      version='0.1',
-      description="Parse about any English textual datetime description",
+      version='0.2',
+      description="parse english textual date descriptions",
       author='Ralf Schmitt',
       author_email='schmir@gmail.com',
       url="http://github.com/schmir/timelib/tree/master",
       ext_modules=[Extension("timelib", sources=sources)],
-      include_dirs=[".",  "ext-date-lib"], 
+      include_dirs=[".",  "ext-date-lib"],
+      **extra
      )
