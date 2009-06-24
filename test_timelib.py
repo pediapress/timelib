c6a55f7fad9b00561d4521b0c279afac58f36a4a
ralf schmitt <schmir@gmail.com>
2009-06-24 14:07:30 +0200

    test epoch

--- test_timelib.py
+++ test_timelib.py
@@ -5,3 +5,7 @@ import py
 
 def test_bad_date():
     py.test.raises(ValueError, timelib.strtotime,  "2007-09-27PM EDT")
+
+def test_epoch():
+    d = timelib.strtotime("1970-01-01")
+    assert d==0, "epoch should be 0"
