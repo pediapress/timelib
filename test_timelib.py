0b4813ff286eeab9e0a3f91ffceee705ab46f2ae
Christoph Kepper <christoph@pediapress.com>
2023-03-17 18:51:45 +0100

    add more tests

--- test_timelib.py
+++ test_timelib.py
@@ -2,7 +2,7 @@
 
 import pytest
 import timelib
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def test_bad_date():
@@ -17,3 +17,20 @@ def test_epoch():
 def test_epoch_plus_one():
     d = timelib.strtodatetime("1970-01-01 00:00:01")
     assert d == datetime(1970, 1, 1, 0, 0, 1), "DateTime should be 1970-01-01 00:00:01"
+
+
+def test_today():
+    d = timelib.strtodatetime("today")
+    assert d == datetime.today().replace(
+        hour=0, minute=0, second=0, microsecond=0
+    ), "DateTime should be today"
+
+
+def test_next_friday():
+    d = timelib.strtodatetime("next friday")
+    today = datetime.today()
+    days_to_friday = (4 - today.weekday()) % 7
+    days_to_friday = 7 if days_to_friday == 0 else days_to_friday
+    next_friday = today + timedelta(days=days_to_friday)
+    next_friday = next_friday.replace(hour=0, minute=0, second=0, microsecond=0)
+    assert d == next_friday, "DateTime should be next friday"
